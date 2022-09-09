031f9068faee7960ae0e1b9192a1f09bbdcde204
Edward Cristi Butoiu <butoiu.edwardcristi@gmail.com>
2022-09-09 22:36:41 +0300

    table-land sdk added w/ a small interop

--- src/app/components/table-land/table-land.component.ts
+++ src/app/components/table-land/table-land.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { connect } from '@tableland/sdk';
 
 @Component({
   selector: 'app-table-land',
@@ -9,7 +10,18 @@ export class TableLandComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  public async ngOnInit(): Promise<void> {
+    //const tableland = await connect({ network: "testnet", chain: "polygon-mumbai", chainId: 5 });
+    const tableland = await connect({ network: "testnet", chainId: 5 });
+    console.log('tableLand', tableland);
 
+    const { name } = await tableland.create(`id int primary key, name text`, { prefix: 'quickstart' })
+    const writeRes = await tableland.write(`INSERT INTO ${name} VALUES (0, 'Bobby Tables')`);
+
+    console.log('writeRes', writeRes);
+
+    const readRes = await tableland.read(`SELECT * FROM ${name}`);
+
+    console.log('readRes', readRes);
+  }
 }
